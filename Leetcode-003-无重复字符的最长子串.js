8c54d03fe05a9fed9a06e3179be1a9c0d17bd3bc
JferLao <43103453+JferLao@users.noreply.github.com>
2020-01-03 14:34:21 +0800

    leetcode

--- Leetcode-003-无重复字符的最长子串.js
+++ Leetcode-003-无重复字符的最长子串.js
@@ -18,6 +18,29 @@ var lengthOfLongestSubstring = function(s) {
    }
     lengthOfLongestSubstring("abcabcaa")
    // 执行结果：通过
-
    // 执行用时 :104 ms, 在所有 javascript 提交中击败了59.72%的用户
    // 内存消耗 :38.1 MB, 在所有 javascript 提交中击败了82.05%的用户
+
+
+var lengthOfLongestSubstring = function(s) {
+				if (s.length < 1) {
+					return 0
+				}
+				let max = 0;
+				let str = ''
+				for (let i = 0; i < s.length; i++) {
+					if (str.indexOf(s[i])<0) {
+						str += s[i]
+						if (str.length > max) {
+							max = str.length
+						}
+					} else {
+						str += s[i]
+						str=str.slice(str.indexOf(s[i])+1)
+					}
+				}
+				return max
+			}
+			// 执行结果：通过
+			// 执行用时 :96 ms ms, 在所有 javascript 提交中击败了77.45%的用户
+			// 内存消耗 :40.8 MB, 在所有 javascript 提交中击败了44.12%的用户
