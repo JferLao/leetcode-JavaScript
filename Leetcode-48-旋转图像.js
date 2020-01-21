e939d1472a8c29991c10b60351c6c9eb64fdc9c5
JferLao <43103453+JferLao@users.noreply.github.com>
2020-01-21 22:43:28 +0800

    Update Leetcode-48-旋转图像.js

--- Leetcode-48-旋转图像.js
+++ Leetcode-48-旋转图像.js
@@ -37,3 +37,34 @@ var rotate = function(matrix) {
 		}
 	}
 };
+
+/*****************************************分割线**********************************/
+
+// 思路三
+var rotate = function(arr) {
+    // 获取n的维度
+    let vecor = arr.length
+        // 垂直翻转,以中心行翻转即可,翻转一半次数即可
+    for (let i = 0, len = vecor / 2; i < len; i++) {
+        // 对每列进行循环遍历
+        for (let j = 0, tmp; j < vecor; j++) {
+            // 保存当前元素
+            tmp = arr[i][j]
+                // 同一列进行依据中心行进行交换,且索引要减1
+            arr[i][j] = arr[vecor - i - 1][j]
+            arr[vecor - i - 1][j] = tmp
+        }
+    }
+    // 对角线翻转
+    for (let i = 0; i < vecor; i++) {
+        for (let j = 0, tmp; j < i; j++) {
+            tmp = arr[i][j]
+                // 根据对角线交换只要对角标对换即可
+            arr[i][j] = arr[j][i]
+            arr[j][i] = tmp
+        }
+    }
+    return arr
+};
+// 执行用时 :60 ms, 在所有 JavaScript 提交中击败了86.80%的用户
+// 内存消耗 :33.7 MB, 在所有 JavaScript 提交中击败了71.69%的用户
