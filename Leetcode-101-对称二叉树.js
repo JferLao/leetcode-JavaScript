7c9321e53d6e5ac75638ed637be8d4deb0e89332
JferLao <43103453+JferLao@users.noreply.github.com>
2020-01-22 12:48:25 +0800

    Update Leetcode-101-对称二叉树.js

--- Leetcode-101-对称二叉树.js
+++ Leetcode-101-对称二叉树.js
@@ -36,3 +36,60 @@ var isSymmetric = function(root) {
     
     return Symmetric(root.left,root.right)
 };
+
+
+// 构造树和实现判断对称二叉树的方法
+//class Node{
+	constructor(val) {
+	    this.val=val
+		this.left=this.right=undefined
+	}
+	static isSymmetric(root){
+		if(!root){
+			return true
+		}
+		let walk=(left,right)=>{
+			if(!left&&!right){
+				return true
+			}
+			if((left&!right)||(!left&&right)||(left.val!==right.val)){
+				return false
+			}
+			return walk(left.left,right.right)&&walk(left.right,right.left)
+		}
+		return walk(root.left,root.right)
+	}
+}
+
+class Tree{
+	constructor(data) {
+	    // 临时存储所有节点方便寻找父子节点
+		let nodeList=[]
+		// 顶节点
+		let root
+		for(let i=0;i<data.length;i++){
+			// 先实现把每个数据转化为节点,再寻找节点之间的关系
+			let node=new Node(data[i])
+			nodeList.push(node)
+			if(i>0){
+				// 计算当前节点属于哪一层,取整()
+				let n=Math.floor(Math.sqrt(i+1))
+				// 记录当前层的起始点
+				let q=Math.pow(2,n)-1
+				// 记录上一层的起始点
+				let p=Math.pow(2,n-1)-1
+				// 找到当前节点的父节点
+				let parent=nodeList[p+Math.floor((i-q)/2)]
+				// 将当前节点和上一层的父节点作关联
+				if(parent.left){
+					parent.right=node
+				}else{
+					parent.left=node
+				}
+			}
+		}
+		root=nodeList.shift()
+		nodeList.length=0
+		return root
+	}
+}
