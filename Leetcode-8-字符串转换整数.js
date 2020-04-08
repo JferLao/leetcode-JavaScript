c0692df47c17dfdc2ecaa5a5c014d92c05e45787
JferLao <43103453+JferLao@users.noreply.github.com>
2020-04-08 23:58:29 +0800

    Update Leetcode-8-字符串转换整数.js

--- Leetcode-8-字符串转换整数.js
+++ Leetcode-8-字符串转换整数.js
@@ -30,3 +30,19 @@ var myAtoi = function(str) {
 	return Math.max(Math.min(parseInt(str) || 0, 2147483647), -2147483648)
 }
 /*************************************分割线*********************************************/
+/**
+ * @param {string} str
+ * @return {number}
+ */
+var myAtoi = function(str) {
+    const res = parseInt(str, 10);
+    if(isNaN(res)){
+        return 0;
+    }else if(res >= Math.pow(2, 31)){
+        return Math.pow(2, 31) - 1;
+    }else if(res <= -1 * Math.pow(2, 31)){
+        return -1 * Math.pow(2, 31);
+    }else{
+        return res;
+    }
+};
